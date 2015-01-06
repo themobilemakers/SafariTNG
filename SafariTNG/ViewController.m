6a4c73e2b2964df8298b677bed247b6e9cd48742
Johnny Appleseed <engineering@mobilemakers.co>
2015-01-06 12:04:16 -0800

    initial commit

--- SafariTNG/ViewController.m
+++ SafariTNG/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -16,12 +20,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.webView.delegate = self;
+    // create string using mobile makers web address
+    NSString *addressString = @"http://mobilemakers.co";
+    // create URL using string from line 22
+    NSURL *addressURL = [NSURL URLWithString:addressString];
+    // create URL request using URL from line 23
+    NSURLRequest *addressRequest = [NSURLRequest requestWithURL:addressURL];
+    // call the method loadRequest on our webView instance to display the webpage
+    [self.webView loadRequest:addressRequest];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+    [self.activityIndicator startAnimating];
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [self.activityIndicator stopAnimating];
 }
 
 @end
